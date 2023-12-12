33728962b6c20c797791c2ad1f2493db31a6d788
Seltam475 <115035889+Seltam475@users.noreply.github.com>
2023-12-12 14:49:52 -0400

    PREGUNTA 1.py

--- SEGUNDO PARCIAL — INF 354/PREGUNTA 1.py
+++ SEGUNDO PARCIAL — INF 354/PREGUNTA 1.py
@@ -1,31 +1,30 @@
-#Este código crea una red neuronal con dos capas, una oculta y una de salida, para clasificar el conjunto de datos Iris. Utiliza la #función de activación sigmoide, el producto punto para propagación hacia adelante y backpropagation para actualizar 
-#los pesos y biases durante la evaluacion.
+#1. Generar una red neuronal (sin librerias) que utilice el dataset iris con producto punto, errores y de dos capas.
 
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-# Función de activación (sigmoid)
+#función de activación
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-# Derivada de la función de activación (sigmoid)
+#derivada de la función de activación
 def sigmoid_derivative(x):
     return x * (1 - x)
 
-# Cargar el conjunto de datos Iris
+#cargar el conjunto de datos Iris
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# Normalizar los datos
+#normalizar los datos
 X = X / np.amax(X, axis=0)
 y = y.reshape(-1, 1)
 
-# Dividir los datos en conjuntos de entrenamiento y prueba
+#dividir los datos en conjuntos de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Inicializar pesos y bias aleatorios para las capas ocultas y de salida
+#inicializar pesos y bias aleatorios para las capas ocultas y de salida
 input_neurons = X.shape[1]
 hidden_neurons = 5
 output_neurons = 1
@@ -36,41 +35,41 @@ hidden_bias = np.random.uniform(size=(1, hidden_neurons))
 output_weights = np.random.uniform(size=(hidden_neurons, output_neurons))
 output_bias = np.random.uniform(size=(1, output_neurons))
 
-# Hiperparámetros
-epochs = 10000
+#Hiperparámetros
+epocas = 10000
 learning_rate = 0.1
 
-# Entrenamiento de la red neuronal
-for epoch in range(epochs):
-    # Forward propagation
+#Entrenamiento de la red neuronal
+for epoca in range(epocas):
+    #propagarcion hacia adelante
     hidden_layer_input = np.dot(X_train, hidden_weights) + hidden_bias
     hidden_layer_output = sigmoid(hidden_layer_input)
     output_layer_input = np.dot(hidden_layer_output, output_weights) + output_bias
     predicted_output = sigmoid(output_layer_input)
 
-    # CalculO DE errores
+    #calculo de errores
     error = y_train - predicted_output
 
-    # Backpropagation
+    #propagacion hacia atras
     d_predicted_output = error * sigmoid_derivative(predicted_output)
     error_hidden_layer = d_predicted_output.dot(output_weights.T)
     d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
 
-    # Actualizar pesos y bias
+    #actualizacion pesos y bias
     output_weights += hidden_layer_output.T.dot(d_predicted_output) * learning_rate
     output_bias += np.sum(d_predicted_output, axis=0, keepdims=True) * learning_rate
     hidden_weights += X_train.T.dot(d_hidden_layer) * learning_rate
     hidden_bias += np.sum(d_hidden_layer, axis=0, keepdims=True) * learning_rate
 
-# Evaluación del modelo
+#se evalua el modelo
 hidden_layer_input = np.dot(X_test, hidden_weights) + hidden_bias
 hidden_layer_output = sigmoid(hidden_layer_input)
 output_layer_input = np.dot(hidden_layer_output, output_weights) + output_bias
 predicted_output = sigmoid(output_layer_input)
 
-# Redondear las predicciones a 0 o 1
+#Redondeo de las predicciones a 0 o 1
 predicted_output = np.round(predicted_output)
 
-# Calcular la precisión
+#Calcular la precision
 accuracy = np.mean(predicted_output == y_test)
 print(f"Precisión en el conjunto de prueba: {accuracy * 100:.2f}%")
